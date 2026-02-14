f4460903875026c02bd31b9da4bac7eaed190df7
gtapiero <giovanny.tapiero@pi.edu.co>
2026-02-14 12:59:46 -0500

    commit - DiagramaController .

--- src/controller/DiagramaController.java
+++ src/controller/DiagramaController.java
@@ -1,6 +1,17 @@
 package controller;
 
+import dao.ClaseUMLDAO;
+
 public class DiagramaController {
-    public void agregarClase(String text) {
+
+    public void agregarClase(String nombreClase) {
+
+        try {
+            ClaseUMLDAO dao = new ClaseUMLDAO();
+            dao.guardarClase(1, nombreClase); // diagrama demo .
+            System.out.println("clase guardada en oracle");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-}
+}   
