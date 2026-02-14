a923c4a91c702b1ddf4bfe896a4ac11b6b76eba7
gtapiero <giovanny.tapiero@pi.edu.co>
2026-02-14 12:41:06 -0500

    commit - ClaseUML / .

---
+++ src/model/ClaseUML.java
@@ -0,0 +1,27 @@
+package model;
+
+public class ClaseUML {
+
+    private int idClase;
+    private String nombre;
+
+    public ClaseUML(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getIdClase() {
+        return idClase;
+    }
+
+    public void setIdClase(int idClase) {
+        this.idClase = idClase;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}   
